53d130e551cf2faf064e5012b659a75e95da4246
Yuset Martinez <ycarballo06@gmail.com>
2024-01-18 21:42:11 -0500

    End of Section 5

---
+++ API/Errors/ErrorController.cs
@@ -0,0 +1,19 @@
+using API.Controllers;
+using API.Errors;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace API.Controllers
+{
+    [Route("errors/{code}")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorController : BaseApiController
+    {
+        public IActionResult Error(int code)
+        {
+            return new ObjectResult(new ApiResponse(code));
+        }
+        
+    }
+}
